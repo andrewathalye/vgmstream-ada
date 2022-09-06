f97072b4de6923f46dda5cb6bf1de2d772234663
Andrew Athalye <andrewathalye@outlook.com>
2022-09-06 18:40:45 +0000

    Add Windows support

--- tools/build_vgmstream.sh
+++ tools/build_vgmstream.sh
@@ -19,8 +19,16 @@ then
 	mkdir build
 	cd build
 	cmake .. -D BUILD_AUDACIOUS=OFF -D BUILD_CLI=OFF -D BUILD_V123=OFF -D USE_ATRAC9=OFF  -D USE_CELT=OFF -D USE_FFMPEG=OFF -D USE_G719=OFF -D USE_G7221=OFF -D USE_JANSSON=OFF -D USE_MAIATRAC3PLUS=OFF -D USE_MPEG=OFF -D USE_SPEEX=OFF -G Ninja || exit 1
-	ninja libvgmstream.so || exit 1
-	mv src/libvgmstream.so ../../ext_lib/
+	case "$OS" in
+		"Windows_NT")
+			ninja libvgmstream.dll || exit 1
+			mv src/libvgmstream.dll ../../ext_lib/
+			;;
+		*)
+			ninja libvgmstream.so || exit 1
+			mv src/libvgmstream.so ../../ext_lib/
+			;;
+	esac
 	cd ../..
 	echo "Done building"
 else
